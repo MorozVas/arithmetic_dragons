25399c2cde7ba8d16afc17046f725439166735ab
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-22 17:53:52 +0300

    ghgf

--- hero.py
+++ hero.py
@@ -6,7 +6,7 @@ from gameunit import *
 
 class Hero(Attacker):
     def __init__(self,name):
-        self._healf = 100
+        self._health = 100
         self._attack = 50
         self._experience = 0
         self._nick=name
