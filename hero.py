648eecf4fbb1cea0315a0afc1a92aea241fe955b
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-22 17:28:41 +0300

    ggh

--- hero.py
+++ hero.py
@@ -2,6 +2,15 @@
 # license: GPLv3
 from gameunit import *
 
+from gameunit import *
+
+class Hero(Attacker):
+    def __init__(self,name):
+        self._healf = 100
+        self._attack = 50
+        self._experience = 0
+        self._nick=name
+
 #FIXME:
 """В этом файле должен быть описан класс героя, унаследованный от Attacker
 Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
