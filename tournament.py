745f1c5b915cdcf0a4c545b383d091f2ddc9bf73
MorozVas <vasiliym2@live.com>
2016-12-06 02:11:32 +0300

    Update tournament.py

--- tournament.py
+++ tournament.py
@@ -13,23 +13,31 @@ def annoying_input_int(message =''):
     return answer
 
 
-def game_tournament(hero, dragon_list):
-    for dragon in dragon_list:
-        print('Вышел', dragon._color, 'дракон!')
-        while dragon.is_alive() and hero.is_alive():
-            print('Вопрос:', dragon.question())
-            answer = annoying_input_int('Ответ:')
+def game_tournament(hero, enemy_list):
+    for enemy in enemy_list:
+            if str(enemy)=='dragon':
+                print('Вышел', enemy._color, 'дракон!')
+            elif str(enemy)=='troll':
+                print('Вышел', enemy._color, 'тролль!')
+            while enemy.is_alive() and hero.is_alive():
+                print('Вопрос:', enemy.question())
+                answer = annoying_input_int('Ответ:')
 
-            if dragon.check_answer(answer):
-                hero.attack(dragon)
-                print('Верно! \n** дракон кричит от боли **')
-                hero.set_experience()
-            else:
-                dragon.attack(hero)
-                print('Ошибка! \n** вам нанесён удар... **')
-        if dragon.is_alive():
-            break
-        print('Дракон', dragon._color, 'повержен!\n')
+                if enemy.check_answer(answer):
+                    hero.attack(enemy)
+                    if str(enemy)=='dragon':
+                        print('Верно! \n** дракон кричит от боли **')
+                        hero.set_experience(15)
+                    elif str(enemy)=='troll':
+                        print('Верно! \n** тролль кричит от боли **')
+                        hero.set_experience(10)
+
+                else:
+                    enemy.attack(hero)
+                    print('Ошибка! \n** вам нанесён удар... **')
+            if enemy.is_alive():
+                break
+            print('Враг повержен!\n')
 
     if hero.is_alive():
         print('Поздравляем! Вы победили!')
@@ -40,15 +48,15 @@ def game_tournament(hero, dragon_list):
 def start_game():
 
     try:
-        print('Добро пожаловать в арифметико-ролевую игру с драконами!')
+        print('Добро пожаловать в арифметико-ролевую игру с драконами и троллями!')
         print('Представьтесь, пожалуйста: ', end = '')
         hero = Hero(input())
 
-        dragon_number = 1
-        dragon_list = generate_dragon_list(dragon_number)
-        assert(len(dragon_list) == dragon_number)
-        print('У Вас на пути', dragon_number, 'драконов!')
-        game_tournament(hero, dragon_list)
+        enemy_number = 3
+        enemy_list = generate_enemy_list(enemy_number)
+        assert(len(enemy_list) == 3)
+        print('У Вас на пути', enemy_number, 'противника!')
+        game_tournament(hero, enemy_list)
 
     except EOFError:
         print('Поток ввода закончился. Извините, принимать ответы более невозможно.')
